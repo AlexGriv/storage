e6b2d4a3b022f18733863e2f83ee128cf845a19a
AlexGriv <lksa-wd@yandex.ru>
2023-03-22 23:50:36 +1000

    db

--- opinions_app.py
+++ opinions_app.py
@@ -1,19 +1,43 @@
 # what_to_watch/opinions_app.py
 
+from datetime import datetime
+
+# Импортируется функция выбора случайного значения
+from random import randrange
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-# Задаётся конкретное значение для конфигурационного ключа
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+
+class Opinion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    text = db.Column(db.Text, unique=True, nullable=False)
+    source = db.Column(db.String(256))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+
 @app.route('/')
 def index_view():
-    return 'Совсем скоро тут будет случайное мнение о фильме!'
+    # Определяется количество мнений в базе данных
+    quantity = Opinion.query.count()
+    # Если мнений нет,
+    if not quantity:
+        # то возвращается сообщение
+        return 'В базе данных мнений о фильмах нет.'
+    # Иначе выбирается случайное число в диапазоне от 0 и до quantity
+    offset_value = randrange(quantity)
+    # И определяется случайный объект
+    opinion = Opinion.query.offset(offset_value).first()
+    return opinion.text
+
 
 if __name__ == '__main__':
     app.run()
