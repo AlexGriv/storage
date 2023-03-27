bae2df6361fb590ddc39f400009b3691787545ff
AlexGriv <lksa-wd@yandex.ru>
2023-03-27 23:08:08 +1000

    ex

--- opinions_app.py
+++ opinions_app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'MY SECRET KEY'
+app.config['SECRET_KEY'] = 'MYSECRETKEY'
 
 db = SQLAlchemy(app)
 
