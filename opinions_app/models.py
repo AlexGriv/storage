bfe4a0e06da49a7cb93f7388af58987e67300df4
AlexGriv <lksa-wd@yandex.ru>
2023-03-29 23:08:52 +1000

    reassembly

---
+++ opinions_app/models.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+from . import db
+
+
+class Opinion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    text = db.Column(db.Text, unique=True, nullable=False)
+    source = db.Column(db.String(256))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    added_by = db.Column(db.String(64))
---
+++ opinions_app/views.py
@@ -0,0 +1,42 @@
+from random import randrange
+
+from flask import abort, flash, redirect, render_template, url_for
+
+from . import app, db
+from .forms import OpinionForm
+from .models import Opinion
+
+
+@app.route('/')
+def index_view():
+    quantity = Opinion.query.count()
+    if not quantity:
+        abort(404)
+    offset_value = randrange(quantity)
+    opinion = Opinion.query.offset(offset_value).first()
+    return render_template('opinion.html', opinion=opinion)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_opinion_view():
+    form = OpinionForm()
+    if form.validate_on_submit():
+        text = form.text.data
+        if Opinion.query.filter_by(text=text).first():
+            flash('Такое мнение уже было оставлено ранее!')
+            return render_template('add_opinion.html', form=form)
+        opinion = Opinion(
+            title=form.title.data,
+            text=form.text.data,
+            source=form.source.data
+        )
+        db.session.add(opinion)
+        db.session.commit()
+        return redirect(url_for('opinion_view', id=opinion.id))
+    return render_template('add_opinion.html', form=form)
+
+
+@app.route('/opinions/<int:id>')
+def opinion_view(id):
+    opinion = Opinion.query.get_or_404(id)
+    return render_template('opinion.html', opinion=opinion)
